c7f98b0841f375db62b349658c3fe68de37b33db
Yevhen Arteshchuk <arteshchuk@gmail.com>
2019-03-26 19:37:12 +0200

    server validate method
    Signed-off-by: Yevhen Arteshchuk <arteshchuk@gmail.com>

--- websocket_test/main.cpp
+++ websocket_test/main.cpp
@@ -1,4 +1,4 @@
-#include <set>
+#include <set>+
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
@@ -11,26 +11,34 @@ using websocketpp::lib::bind;
 
 class base_server {
 public:
-    base_server() {
+    base_server() : is_connected(false) {
         m_server.init_asio();
         m_server.set_open_handler(bind(&base_server::on_open,this,::_1));
         m_server.set_close_handler(bind(&base_server::on_close,this,::_1));
+        m_server.set_validate_handler(bind(&base_server::on_validate,this,::_1));
         m_server.set_message_handler(bind(&base_server::on_message,this, ::_1,::_2));
     }
 
     void on_open(connection_hdl hdl) {
-        m_connections.insert(hdl);
+        is_connected = true;
         for (int i = 0; i < 15; i++) {
-            m_server.send(*m_connections.begin(), &i, 4, websocketpp::frame::opcode::binary);
+            m_server.send(hdl, &i, 4, websocketpp::frame::opcode::binary);
         }
     }
 
     void on_close(connection_hdl hdl) {
-        m_connections.erase(hdl);
+        is_connected = false;
     }
 
     void on_message(connection_hdl hdl, server::message_ptr msg) {
 
+    }
+
+    bool on_validate(connection_hdl hdl) {
+        if (is_connected) {
+            return false;
+        }
+        return true;
     }
 
     void run(uint16_t port) {
@@ -39,10 +47,9 @@ public:
         m_server.run();
     }
 private:
-    typedef std::set<connection_hdl,std::owner_less<connection_hdl>> con_list;
-
     server m_server;
-    con_list m_connections;
+    bool is_connected;
+
 };
 
 int main() {
